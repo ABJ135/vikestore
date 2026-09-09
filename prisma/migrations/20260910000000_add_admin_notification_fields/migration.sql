16829ec50db9f8841a0884c814d5701aa61b6f7d
abj135 <1515.abubakarjaved.m2@gmail.com>
2026-09-09 22:47:26 +0500

    fix migration.sql syntax for admin notification fields

--- prisma/migrations/20260910000000_add_admin_notification_fields/migration.sql
+++ prisma/migrations/20260910000000_add_admin_notification_fields/migration.sql
@@ -1,4 +1,2 @@
-@"
 ALTER TABLE "Admin" ADD COLUMN "notifyLowStock" BOOLEAN NOT NULL DEFAULT true;
 ALTER TABLE "Admin" ADD COLUMN "notifyNewOrder" BOOLEAN NOT NULL DEFAULT true;
-"@ | Out-File -Encoding utf8 prisma\migrations\20260910000000_add_admin_notification_fields\migration.sql
