07d5d347d9e7e005fe2c25da1ba684f479978333
abj135 <1515.abubakarjaved.m2@gmail.com>
2026-09-10 21:11:30 +0500

    added cors for customer

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN,
+    origin: [process.env.CORS_ORIGIN, process.env.CORS_ORIGIN2],
     credentials: true,
   });
 
