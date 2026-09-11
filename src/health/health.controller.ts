ce47d1c6a525666cea2234e7aa67dab8e0da22ad
abj135 <1515.abubakarjaved.m2@gmail.com>
2026-09-11 21:59:56 +0500

    make the health controller public

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from '../common/decorators/public.decorator';
 
 @Controller('health')
 export class HealthController {
-  @Get()
-  check() {
-    return { status: 'ok', timestamp: new Date().toISOString() };
-  }
+    @Public()
+    @Get()
+    check() {
+        return { status: 'ok', timestamp: new Date().toISOString() };
+    }
 }
